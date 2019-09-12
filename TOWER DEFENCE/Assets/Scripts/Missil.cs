88ad0ff49c25ef6ab48b4690f9345b712c0c7446
FranciscoSabato <francisco_francisco123@outlook.com>
2019-09-12 19:31:30 -0300

    primeiro upload

---
+++ TOWER DEFENCE/Assets/Scripts/Missil.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missil : MonoBehaviour
+{
+    private float velocidade = 3f;
+    public GameObject alvo;
+    [SerializeField] private int pontosDeDano;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Anda();
+        AlteraDirecao();
+    }
+
+    private void Start()
+    {
+        alvo = GameObject.Find("Inimigo");
+        AutoDestroiDeposDeSegundos(5f);
+    }
+
+    private void AlteraDirecao()
+    {
+        Vector3 posicaoAtual = transform.position;
+        Vector3 posicaoDoAlvo = alvo.transform.position;
+        Vector3 direcaoDoAlvo = posicaoDoAlvo - posicaoAtual;
+        transform.rotation = Quaternion.LookRotation(direcaoDoAlvo);
+    }
+
+    private void Anda()
+    {
+        Vector3 posicaoAtual = transform.position;
+        Vector3 deslocamento = transform.forward * Time.deltaTime *velocidade;
+        transform.position = posicaoAtual + deslocamento;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Inimigo"))
+        {
+            Destroy(this.gameObject);
+            //Destroy(other.gameObject);
+            Inimigo inimigo = other.GetComponent<Inimigo>();
+            inimigo.RecebeDano(pontosDeDano);
+        }
+        
+    }
+
+    private void AutoDestroiDeposDeSegundos(float segundos)
+    {
+        Destroy(this.gameObject, segundos);
+
+    }
+}
---
+++ TOWER DEFENCE/Assets/Scripts/Torre.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torre : MonoBehaviour
+{
+    public GameObject projetilPrefab;
+
+    public float tempoDeRecarga = 2f;
+
+    private float momentoDoUltimoDisparo;
+
+    void Update()
+    {
+        Atira();
+    }
+
+    private void Atira()
+    {
+        float tempoAtual = Time.time;
+        if (tempoAtual > momentoDoUltimoDisparo + tempoDeRecarga)
+        {
+            momentoDoUltimoDisparo = tempoAtual;
+            GameObject pontoDeDisparo = this.transform.Find("CanhaoDaTorre/PontoDeDisparo").gameObject;
+            Vector3 posicaoDoPontoDeDisparo = pontoDeDisparo.transform.position;
+            Instantiate(projetilPrefab, posicaoDoPontoDeDisparo, Quaternion.identity);
+        }
+    }
+}
